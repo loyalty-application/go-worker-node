252b40fa4cb73dc6504e9d3473a20eb958ca2b8a
Jing kai Ooi <ooijingkai@gmail.com>
2023-03-15 13:54:32 +0800

    cleanup

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/cs301-itsa/project-2022-23t2-project-2022-23t2-g1-t6-worker-se
 go 1.18
 
 require (
-	github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
-	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.8.2 // indirect
+	github.com/confluentinc/confluent-kafka-go v1.9.2
+	github.com/linkedin/goavro/v2 v2.12.0
 )
+
+require github.com/golang/snappy v0.0.4 // indirect
--- main.go
+++ main.go
@@ -16,32 +16,39 @@ func main() {
 		"client.id":                "FtpProcessing",
 		"enable.auto.commit":       false,
 		"enable.auto.offset.store": false,
-		"isolation.level":"read_committed",
+		"auto.offset.reset":        "earliest",
+		"isolation.level":          "read_committed",
 	})
 
-	topic := "transactions"
-	
-	err = consumer.Assign([]kafka.TopicPartition{{Topic: &topic, Partition: 1,Offset: kafka.OffsetStored}})
+	topic := "ftptransactions"
+
+	consumer.Subscribe(topic, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	// low,_,err := consumer.QueryWatermarkOffsets(topic,2,2000)
-
 
 	run := true
-	
 
 	for run {
+
 		msg, err := consumer.ReadMessage(time.Second)
+
 		if err == nil {
+			// native, _, err := codec.NativeFromBinary(msg.Value)
+			// if err != nil {
+			// 	fmt.Println("Error decoding Avro message value:", err)
+			// 	continue
+			// }
+			
 			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+
+			// Only commit after successfully processed the message
 			consumer.CommitMessage(msg)
 		} else if err != nil {
-		// The client will automatically try to recover from all errors.
-		// Timeout is not considered an error because it is raised by
-		// ReadMessage in absence of messages.
-			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+			// The client will automatically try to recover from all errors.
+			// Timeout is not considered an error because it is raised by
+			// ReadMessage in absence of messages.
+			// fmt.Printf("Consumer error: %v (%v)\n", err, msg)
 		}
 	}
 
