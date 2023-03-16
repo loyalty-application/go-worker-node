f0d07aadcf1816574158d067fa6cf488ce12e1cd
hongyao38 <hongyao.lim.2021@scis.smu.edu.sg>
2023-03-16 21:27:33 +0800

    moved methods from gabriel

--- main.go
+++ main.go
@@ -15,6 +15,62 @@ import (
  * 3. Implement reading from Kafka
  */
 
+func CreateTransactions(userId string, transactions models.TransactionList) (result *mongo.InsertManyResult, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// convert from slice of struct to slice of interface
+	t := make([]interface{}, len(transactions.Transactions))
+	for i, v := range transactions.Transactions {
+		v.UserId = userId
+		t[i] = v
+	}
+
+	result, err = transactionCollection.InsertMany(ctx, t)
+	return result, err
+
+}
+
+// @Summary Create Transactions for User
+// @Description Create transaction records
+// @Tags    transaction
+// @Accept  application/json
+// @Produce application/json
+// @Param   Authorization header string true "Bearer eyJhb..."
+// @Param   user_id path string true "user's id"
+// @Param   request body models.TransactionList true "transactions"
+// @Success 200 {object} []models.Transaction
+// @Failure 400 {object} models.HTTPError
+// @Router  /transaction/{user_id} [post]
+func (t TransactionController) PostTransactions(c *gin.Context) {
+	userId := c.Param("userId")
+	if userId == "" {
+		c.JSON(http.StatusBadRequest, models.HTTPError{http.StatusBadRequest, "Invalid User Id"})
+		return
+	}
+
+	data := new(models.TransactionList)
+	err := c.BindJSON(data)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, models.HTTPError{http.StatusBadRequest, "Invalid Transaction Object" + err.Error()})
+		return
+	}
+
+	// TODO: make this operation atomic https://www.mongodb.com/docs/drivers/go/current/fundamentals/transactions/
+	result, err := CreateTransactions(userId, *data)
+	if err != nil {
+		msg := "Invalid Transactions"
+		if mongo.IsDuplicateKeyError(err) {
+			msg = "transaction_id already exists"
+		}
+		c.JSON(http.StatusBadRequest, models.HTTPError{http.StatusBadRequest, msg})
+		return
+	}
+
+	c.JSON(http.StatusOK, result)
+}
+
+
 func main() {
 
 	// Setting up a connection with kafka
@@ -45,11 +101,6 @@ func main() {
 		msg, err := consumer.ReadMessage(time.Second)
 
 		if err == nil {
-			// native, _, err := codec.NativeFromBinary(msg.Value)
-			// if err != nil {
-			// 	fmt.Println("Error decoding Avro message value:", err)
-			// 	continue
-			// }
 			
 			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
 
