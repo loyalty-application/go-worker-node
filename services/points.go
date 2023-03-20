3115a7d00330accd218f7672a0848add99563ebc
hongyao38 <hongyao.lim.2021@scis.smu.edu.sg>
2023-03-20 07:20:54 +0000

    Implemented points conversion

--- services/points.go
+++ services/points.go
@@ -30,9 +30,6 @@ func In(arr []int, target int) bool {
 
 func ConvertPoints(transaction *models.Transaction) {
 
-	// MCC code categorization (SUBJECT TO CHANGES)
-	var ONLINE_SHOPPING = []int{5999, 5964, 5691, 5311, 5411, 5399}
-	// var HOTEL = []int{7011}
 	var EXCLUDED_MCCS = []int{6051, 9399, 6540}
 
 	// If MCC is to be excluded, do not convert to points
@@ -41,30 +38,86 @@ func ConvertPoints(transaction *models.Transaction) {
 		return
 	}
 
-	// Get amount spent
+	// Get amount spent, if in foreign currency, convert to SGD
 	amountSpent := transaction.Amount * GetExchangeRate(transaction.Currency)
 
 	// Conversion: $$ -> POINTS
 	if transaction.CardType == "scis_shopping" {
-		pointsConversionRate := 4
-		if In(ONLINE_SHOPPING, mcc) {  // Bonus for online shopping
-			pointsConversionRate = 10
-		}
-		transaction.Points = amountSpent * float64(pointsConversionRate + 1)
-
+		calculatePoints(transaction, amountSpent)
+		
 	// Conversion: $$ -> CASHBACK
 	} else if transaction.CardType == "scis_freedom" {
-		cashBack := 0.005 * amountSpent
-		if amountSpent > 500 {
-			cashBack += 0.01 * amountSpent
-		}
-		if amountSpent > 2000 {
-			cashBack += 0.03 * amountSpent
-		}
-		transaction.CashBack = cashBack
+		calculateCashBack(transaction, amountSpent)
 
 	// Conversion: $$ -> MILES
 	} else {
-		transaction.Miles = 0
+		calculateMiles(transaction, amountSpent)
 	}
+
+	// INSERT: Apply campaign benefits
+}
+
+
+func calculatePoints(transaction *models.Transaction, amountSpent float64) {
+
+	// MCC code categorization
+	var ONLINE_SHOPPING = []int{5999, 5964, 5691, 5311, 5411, 5399, 5815, 5816, 5817, 5818}
+	var SHOPPING = []int{
+		4816, 5045, 5262, 5309, 5310, 5311, 5331, 5399, 5611, 5621, 5631, 5641, 5651, 5655, 5661, 
+		5691, 5699, 5732, 5733, 5734, 5735, 5912, 5942, 5944, 5945, 5946, 5947, 5948, 5949, 5964, 
+		5965, 5966, 5967, 5968, 5969, 5970, 5992, 5999, 5621, 5631}
+
+	mcc, _ := strconv.Atoi(transaction.MCC)
+
+	pointsConversionRate := 1  // Base conversion rate for all spend types
+
+	// Bonus for online shopping
+	if In(ONLINE_SHOPPING, mcc) {
+		pointsConversionRate = 10
+
+	// Bonus for shopping
+	} else if In(SHOPPING, mcc) {
+		pointsConversionRate = 4
+	}
+	
+	transaction.Points = amountSpent * float64(pointsConversionRate)
+}
+
+func calculateCashBack(transaction *models.Transaction, amountSpent float64) {
+	cashBack := 0.05 * amountSpent
+	if amountSpent > 2000 {
+		cashBack = 0.03 * amountSpent
+	} else if amountSpent > 500 {
+		cashBack = 0.01 * amountSpent
+	}
+	transaction.CashBack = cashBack
+}
+
+func calculateMiles(transaction *models.Transaction, amountSpent float64) {
+
+	var HOTEL = []int{7011}
+	mcc, _ := strconv.Atoi(transaction.MCC)
+
+	// Get base rate for premium vs platinum
+	milesConversionRate := 1.1
+	if transaction.CardType == "scis_platinummiles" {
+		milesConversionRate = 1.4
+	}
+
+	// If spend type is foreign
+	if transaction.Currency != "SGD" {
+		// Base conversion rate for foreign card spend
+		milesConversionRate = 2.2
+		if transaction.CardType == "scis_platinummiles" {
+			milesConversionRate = 3
+
+			// If it is a foreign hotel spend
+			if In(HOTEL, mcc) { milesConversionRate = 6 }
+		}
+		
+	// For local hotel spends
+	} else if In(HOTEL, mcc) {
+		milesConversionRate = 3
+	}
+	transaction.Miles = amountSpent * milesConversionRate
 }
