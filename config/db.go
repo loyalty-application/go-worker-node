c22fa3bf247449cedbf05bde0a522c079fedc51d
soonann <sudo@soonann.dev>
2023-03-28 02:35:11 +0800

    fix: changed to connection from go-gin-backend

--- config/db.go
+++ config/db.go
@@ -22,24 +22,75 @@ func DBinstance() (client *mongo.Client) {
 	host := os.Getenv("MONGO_HOST")
 	port := os.Getenv("MONGO_PORT")
 
-	replicaSet := "replica-set"
-	if os.Getenv("GIN_MODE") == "release" {
-		replicaSet = "rs0"
-	}
-	conn := fmt.Sprintf("mongodb://%s:%s@%s:%s/?replicaSet=%s", user, pass, host, port, replicaSet)
+	conn := fmt.Sprintf("mongodb://%s:%s@%s:%s", user, pass, host, port)
 
-	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
-	clientOptions := options.Client().ApplyURI(conn).SetServerAPIOptions(serverAPIOptions)
+	replicaSetQueryString := "/?replicaSet=replica-set"
+	tlsQueryString := ""
+	secondaryQueryString := ""
+	//var tlsConfig *tls.Config
+
+	if os.Getenv("GIN_MODE") == "release" {
+		replicaSetQueryString = "/?replicaSet=rs0"
+		//tlsQueryString = "&tls=true"
+		secondaryQueryString = "&readPreference=secondaryPreferred&retryWrites=false"
+
+		//// configure tls
+		//var filename = "rds-combined-ca-bundle.pem"
+		//tlsConfig := new(tls.Config)
+		//certs, err := ioutil.ReadFile(filename)
+
+		//if err != nil {
+		//fmt.Println("Failed to read CA file")
+		//return
+		//}
+
+		//tlsConfig.RootCAs = x509.NewCertPool()
+		//ok := tlsConfig.RootCAs.AppendCertsFromPEM(certs)
+
+		//if !ok {
+		//fmt.Println("Failed to append CA file")
+		//return
+		//}
+
+		//if tlsConfig != nil {
+		//fmt.Println("Successfully set TLS config")
+		//clientOptions.SetTLSConfig(tlsConfig)
+		//}
+
+	}
+	conn = fmt.Sprintf("%s%s%s%s", conn, replicaSetQueryString, tlsQueryString, secondaryQueryString)
+
+	fmt.Printf("Attempting connection with: %s\n", conn)
+	//serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
+	//clientOptions := options.Client().ApplyURI(conn).SetServerAPIOptions(serverAPIOptions)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	// connect to mongodb
-	client, err := mongo.Connect(ctx, clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Connected to MongoDB!")
 
-	// initialise indexes
+	// connect to mongodb
+	//client, err := mongo.Connect(ctx, clientOptions)
+	//if err != nil {
+	//log.Fatal(err)
+	//}
+	client, err := mongo.NewClient(options.Client().ApplyURI(conn))
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	fmt.Println("Connecting to MongoDB ...")
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatalf("Failed to connect to cluster: %v", err)
+	}
+
+	fmt.Println("Success!")
+	// Force a connection to verify our connection string
+
+	fmt.Println("Pinging server ...")
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("Failed to ping cluster: %v", err)
+	}
+	fmt.Println("Success!")
 
 	return client
 }
