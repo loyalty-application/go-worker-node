4ff628c486de5a41de1f8063db71a8edb6017722
Gabriel Ong <ojh809@gmail.com>
2023-03-28 16:30:06 +0800

    chore: fixed struct tag for transactions

--- models/transaction.go
+++ models/transaction.go
@@ -5,7 +5,7 @@ type TransactionList struct {
 }
 
 type Transaction struct {
-	Id              string     `json:"id" bson:"id" example:"1"`
+	Id              string  `json:"id" bson:"id" example:"1"`
 	TransactionId   string  `json:"transaction_id" bson:"transaction_id" example:"txn00001"`
 	Merchant        string  `json:"merchant" bson:"merchant" example:"7-11"`
 	MCC             string  `json:"mcc" bson:"mcc" example:"5311"`
@@ -14,8 +14,8 @@ type Transaction struct {
 	TransactionDate string  `json:"transaction_date" bson:"transaction_date" example:"yyyy-mm-dd hh:mm:ss"`
 	CardId          string  `json:"card_id" bson:"card_id" example:"4111222233334444"`
 	CardPan         string  `json:"card_pan" bson:"card_pan" example:"xyz"`
-	CardType        string  `json:"card_type" bson:"card_type" example"super_miles_card"`
-	Points          float64 `json:"points bson: points" example 4.5`
-	Miles           float64 `json:"miles bson: miles" example 10.1`
-	CashBack        float64 `json:"cash_back bson cash_back" example 30.45`
+	CardType        string  `json:"card_type" bson:"card_type" example:"super_miles_card"`
+	Points          float64 `json:"points" bson:"points" example:"4.5"`
+	Miles           float64 `json:"miles" bson:"miles" example:"10.1"`
+	CashBack        float64 `json:"cash_back" bson:"cash_back" example:"30.45"`
 }
